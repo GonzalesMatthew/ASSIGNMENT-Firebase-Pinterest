130e06003967b3903588e1175075c31475dfc9d5
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-09 21:29:32 -0600

    fixed getBoards errors where it looked for uid instead of the boardId it requires

--- src/javascripts/helpers/data/pinData.js
+++ src/javascripts/helpers/data/pinData.js
@@ -1,4 +1,3 @@
-import firebase from 'firebase/app';
 import 'firebase/auth';
 import axios from 'axios';
 import firebaseConfig from '../apiKeys';
@@ -13,25 +12,6 @@ const getBoardsPins = (boardId) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-// DELETE PINS
-const deletePin = (firebaseKey, boardId) => new Promise((resolve, reject) => {
-  axios.delete(`${dbUrl}/pins/${firebaseKey}.json`)
-    .then(() => getBoardsPins(boardId).then((pinsArray) => resolve(pinsArray)))
-    .catch((error) => reject(error));
-});
-
-// CREATE PIN
-const createPin = (pinObject, uid) => new Promise((resolve, reject) => {
-  axios.post(`${dbUrl}/pins.json`, pinObject)
-    .then((response) => {
-      const body = { firebaseKey: response.data.name };
-      axios.patch(`${dbUrl}/pins/${response.data.name}.json`, body)
-        .then(() => {
-          getBoardsPins(uid).then((pinsArray) => resolve(pinsArray));
-        });
-    }).catch((error) => reject(error));
-});
-
 // GET SINGLE PIN
 const getSinglePin = (firebaseKey) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/pins/${firebaseKey}.json`)
@@ -39,10 +19,31 @@ const getSinglePin = (firebaseKey) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+// DELETE PINS
+const deletePin = (firebaseKey) => new Promise((resolve, reject) => {
+  getSinglePin(firebaseKey).then((pinInfo) => {
+    axios.delete(`${dbUrl}/pins/${firebaseKey}.json`)
+      .then(() => getBoardsPins(pinInfo.board_id).then((pinsArray) => resolve(pinsArray)))
+      .catch((error) => reject(error));
+  });
+});
+
+// CREATE PIN
+const createPin = (pinObject) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/pins.json`, pinObject)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/pins/${response.data.name}.json`, body)
+        .then(() => {
+          getBoardsPins(pinObject.board_id).then((pinsArray) => resolve(pinsArray));
+        });
+    }).catch((error) => reject(error));
+});
+
 // UPDATE PIN
 const updatePin = (firebaseKey, pinObject) => new Promise((resolve, reject) => {
   axios.patch(`${dbUrl}/pins/${firebaseKey}.json`, pinObject)
-    .then(() => getBoardsPins(firebase.auth().currentUser.uid))
+    .then(() => getBoardsPins(pinObject.board_id))
     .then((pinsArray) => resolve(pinsArray))
     .catch((error) => reject(error));
 });
