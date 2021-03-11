34355b14add7015b3f51a38b32d25c37145e044b
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-10 19:34:54 -0600

    beefed-up my boards styling

--- src/javascripts/components/boards.js
+++ src/javascripts/components/boards.js
@@ -1,22 +1,26 @@
 const showBoards = (array) => {
   document.querySelector('#app').innerHTML = `
-  <button class="btn btn-success" id="add-board">Create New Board</button>
-    <h1>Boards</h1>
-    <div id="user-container" class="row justify-content-center"></div>`;
+  <button class="my-4 btn btn-success" id="add-board">Create New Board</button>
+    <div id="container">
+      <h1>Boards</h1>
+      <div id="user-container" class="row justify-content-center p-5 mx-5 mt-3 mb-5 rounded shadow-lg">
+      </div>
+    </div>`;
 
   array.forEach((item) => {
     document.querySelector('#user-container').innerHTML += `
-    <div class="col-sm-6">
-      <div class="card">
+    <div class="col-sm-3">
+      <div class="card v-25 m-3 p-4 shadow-lg rounded">
         <h5 class="card-title">${item.title}</h5>
-        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
-        <div class="card-body" style="height: 180px;">
+        <img class="card-img-top px-2 shadow-lg rounded overflow-hidden" src=${item.image} alt=${item.title} style="height:200px;">
+        <div class="card-body">
           <hr>
-          <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="view-board-btn--${item.firebaseKey}">View Board</button>
-          <button class="btn btn-danger" id="delete-board--${item.firebaseKey}">Delete Board</button>
+          <button class="btn btn-info shadow-lg" data-toggle="modal" data-target="#formModal" id="view-board-btn--${item.firebaseKey}">View Board</button>
+          <button class="btn btn-danger shadow-lg" id="delete-board--${item.firebaseKey}">Delete Board</button>
         </div>
       </div>
-    </div>`;
+    </div>
+    `;
   });
 };
 
--- src/javascripts/components/navBar.js
+++ src/javascripts/components/navBar.js
@@ -1,7 +1,7 @@
 const navBar = () => {
   document.querySelector('#navigation').innerHTML = `
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-4">
-        <a class="navbar-brand title" href="#">Firebase Pinterest</a>
+        <a id='brand' class="navbar-brand title" href="#">Firebase Pinterest</a>
     </nav>
     `;
 };
