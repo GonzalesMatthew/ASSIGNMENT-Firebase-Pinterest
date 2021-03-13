ffb8d2a919a3ce82c473b82ef8eeec43de328d95
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-13 02:24:37 -0600

    dark mode toggle button which changes background color to black

--- src/javascripts/components/navBar.js
+++ src/javascripts/components/navBar.js
@@ -1,8 +1,9 @@
 const navBar = () => {
   document.querySelector('#navigation').innerHTML = `
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-4">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark mb-4 d-flex">
       <a id='brand' class="navbar-brand title" href="#">Firebase Pinterest</a>
       <input id="search-bar" placeholder="Search Boards" aria-label="Search bar for boards">
+      <btn type='button' id='dark-mode' class='btn btn-dark btn-sm ml-auto'>Dark Mode</btn>
     </nav>
     `;
 };
